b25fd537ae4620b7b14a102bf946acb06038e676
James P <93916271+jaminthecloud@users.noreply.github.com>
2023-12-01 14:19:13 +0000

    skybox

---
+++ Assets/InputManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializedField]
+    private Camera sceneCamera;
+
+    private Vector3 lastPosition;
+
+    [SerializedField]
+    private LayerMask placementLayermask;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
